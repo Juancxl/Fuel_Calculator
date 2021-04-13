956cfc5ab2d8ad6ecfa00da3e7f55157baa586fe
Juancxl <68575481+Juancxl@users.noreply.github.com>
2021-04-13 16:28:18 -0300

    V1.2

--- fuelCalculator.py
+++ fuelCalculator.py
@@ -11,11 +11,11 @@ import math
 sg.theme("Dark Grey")
 
 layout = [
-    [sg.Text("Leader's race pace [min:sec.dec]: "), sg.Input(key='lapString')],
+    [sg.Text("Leader's race pace [min:sec]: "), sg.Input(key='lapString')],
     [sg.Text('Race lenght [minutes]: '), sg.Input(key='raceTime')],
     [sg.Text('Fuel consumption [L/Lap]: '), sg.Input(key='fuelConsumption')],   
     [sg.Button('Calculate!')],
-    [sg.Output(size = (50, 3), font = '\b',key= '-OUTPUT-')],
+    [sg.Output(size = (40, 5), font = '\b',key= '-OUTPUT-')],
 ]
 
 
@@ -33,18 +33,24 @@ while True:
     if events == 'Calculate!':
         window['-OUTPUT-'].update(value='')             # Clearing the output window
 
-        minutes = values['lapString'].split(':', -1)[0]
-        seconds = values['lapString'].split(':', -1)[1]
+        try:                                            # Reading the user inputs
+            minutes = values['lapString'].split(':', -1)[0]
+            seconds = values['lapString'].split(':', -1)[1]
 
-#        if type(minutes) != int and type(seconds) != int:
-#            print(f'Error reading data. Please review your inputs.')
+            if len(minutes) == 1 and len(seconds) == 2 and minutes.isnumeric() and seconds.isnumeric():    # Checking if the input data format is correct
+                averageLapInSeconds = (float(minutes) * 60) + float(seconds)            # If yes, then the app will do his thing
+                raceTimeInSeconds = int(values['raceTime']) * 60
+                lapsNumber = raceTimeInSeconds/averageLapInSeconds
+                totalFuel = lapsNumber * float(values['fuelConsumption'])
+                safetyFuel = totalFuel + float(values['fuelConsumption'])               # Plus one lap of fuel, for safety
+                print(f'Estimated race laps: {math.ceil(lapsNumber)} laps')
+                print(f'Total race fuel needed: {math.ceil(totalFuel)} Liters')
+                print(f'Safety ammount of fuel: {math.ceil(safetyFuel)} Liters')
 
-        averageLapInSeconds = (float(minutes) * 60) + float(seconds)
+            else:
+                print(f'Data formats are wrong. Please review your inputs.')            # If not, the user will need to check his inputs
 
-        raceTimeInSeconds = int(values['raceTime']) * 60
-
-        lapsNumber = raceTimeInSeconds/averageLapInSeconds
-        totalFuel = lapsNumber * float(values['fuelConsumption'])
-
-        print(f'Estimated race laps: {math.ceil(lapsNumber)} laps')
-        print(f'Total race fuel needed: {math.ceil(totalFuel)} Liters')
+        except IndexError:                              # Something went wrong with the user inputs
+            print(f'Error reading data. Please review your inputs.')
+        except ValueError:                              # Something went wrong with the user inputs (2)
+            print(f'Error reading data. Please review your inputs 1.')
